e49a2347153a1f9fdac13f0279512916ba0b5f42
Asad Ali Baloch <asad.baloch@10pearls.com>
2021-12-15 12:32:37 +0500

    feat: adding the types for future use

---
+++ src/shared/Form/types.ts
@@ -0,0 +1,23 @@
+// Basic form data model
+export type TFormDataProp = TFormDataObject[];
+export type TFormDataObject = {
+  name: string;
+  label?: string;
+  value?: TFormInputValue;
+  validationType?: string;
+  validations?: TFormInputValidation[];
+};
+
+// Input type
+export type TInputType = "text"; // can be made generic for future
+export type TFormInputValue = string | number; // Default value provided by user to the input
+
+// Validation type
+export type TFormInputValidation = {
+  type: string;
+  params: (string | number)[];
+};
+
+// Error type
+export type TInputError = string; // error message to be shown with the label
+export type TInputErrors = TInputError[];
